56268f695e7182dc2ca06f292c55c072f726ab83
shinminji <tlsalswl0630@gamil.com>
2020-09-06 02:56:07 +0900

    ~ch2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
+
+const ghibliILike = [
+  {
+    id:1,
+    name: "Totoro",
+    image: "https://i.pinimg.com/564x/ef/ef/e4/efefe4ee1e559e54085bc8cc33d02897.jpg",
+    rating: 5,
+  },
+  {
+    id:2,
+    name: "Haul",
+    image: "https://pbs.twimg.com/profile_images/414670557779464193/ZcPavNDU.jpeg",
+    rating: 4.9,
+  },
+  {
+    id:3,
+    name: "Ponyo",
+    image: "https://img1.daumcdn.net/thumb/C400x400/?fname=http://t1.daumcdn.net/brunch/service/user/40yV/image/57msSqM71XQfFFReQZbkJm3c0Hg.jpg",
+    rating: 4.8,
+  },
+]
+
+Ghibli.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {ghibliILike.map(go => (
+        <Ghibli
+         key={go.id}
+         name={go.name}
+         picture={go.image} 
+         rating={go.rating} />
+      ))}
+    </div>
+  );
+}
+
+function Ghibli({ name, picture, rating }){
+  return(
+    <div>
+    <h1>I like {name} !</h1>
+    <h4>{rating}/5.0</h4>
+    <img src={picture} alt={name}/>
     </div>
   );
 }
